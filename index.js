8ef1941505fe90d9f52d7df38ec4014c7e992285
ShubhamPalriwala <spalriwalau@gmail.com>
2021-12-08 18:56:58 +0530

    feat: poc

--- index.js
+++ index.js
@@ -1,15 +1,97 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const axios = require("axios");
+
+const lightsPerAxis = 4;
+let commits = 0;
+let issues = 0;
+let pullrequests = 0;
+let codereviews = 0;
+
+let commitLights = 0;
+let issueLights = 0;
+let pullrequestLights = 0;
+let codereviewLights = 0;
+
+const response = {
+  commitAxis: commitLights,
+  issueAxis: issueLights,
+  pullrequestAxis: pullrequestLights,
+  codereviewAxis: codereviewLights,
+};
+
+const getUserData = async (username, ghToken) => {
+  const response = await axios({
+    method: "get",
+    url: `https://api.github.com/users/shubhampalriwala/events`,
+    headers: {
+      Authorization: `Bearer ${ghToken}`,
+    },
+    params: {
+      per_page: 100,
+    },
+  });
+  response.data.forEach((ghEvent) => {
+    incrementType(ghEvent);
+  });
+  getRatio(commits, issues, pullrequests, codereviews);
+};
+
+const incrementType = (ghEvent) => {
+  switch (ghEvent.type) {
+    case "PushEvent":
+      commits++;
+      break;
+    case "IssuesEvent":
+      if (ghEvent.payload.action === "opened") {
+        issues++;
+      }
+      break;
+    case "PullRequestEvent":
+      if (ghEvent.payload.action === "opened") {
+        pullrequests++;
+      }
+      break;
+    case "PullRequestReviewEvent":
+      if (ghEvent.payload.action === "created") {
+        codereviews++;
+      }
+      break;
+  }
+};
+
+const getRatio = (commits, issues, pullrequests, codereviews) => {
+  console.log(commits, issues, pullrequests, codereviews);
+  const total = commits + issues + pullrequests + codereviews;
+
+  commitLights = Math.ceil((commits / total) * lightsPerAxis);
+  issueLights = Math.ceil((issues / total) * lightsPerAxis);
+  pullrequestLights = Math.ceil((pullrequests / total) * lightsPerAxis);
+  codereviewLights = Math.ceil((codereviews / total) * lightsPerAxis);
+
+  response.commitAxis = commitLights;
+  response.issueAxis = issueLights;
+  response.pullrequestAxis = pullrequestLights;
+  response.codereviewAxis = codereviewLights;
+};
 
 try {
-  const nameToGreet = core.getInput("my-username");
-  console.log(`Hello ${nameToGreet}`);
+  const username = core.getInput("my-username");
+  console.log(`Hello ${username}`);
   const time = new Date().toTimeString();
   console.log(time);
   core.setOutput("time", time);
 
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+  getUserData(username, GITHUB_TOKEN)
+    .then(() => {
+      console.log(response);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 } catch (error) {
+  // console.log(error);
   core.setFailed(error.message);
 }
